250629f7ce0fc1b8110ef9cc1cec1d6444d7a299
harelMiz <harel10mizrahi@gmail.com>
2025-04-15 20:03:32 +0300

    adding Add Test to test file

--- test.cpp
+++ test.cpp
@@ -1,10 +1,17 @@
 #include <gtest/gtest.h>
-#include "math.h"
-
-TEST(AdditionTest, PositiveNumbers) {
-    EXPECT_EQ(add(1, 2), 3);
+#include "bloomFilter.hpp"
+/*      
+Smoke test for the add function of bloomFilter.
+This test ensures that calling add with various strings (including empty and URL) does not crash or throw exceptions.
+*/
+TEST(AddTest, Test1) {
+    bloomFilter bf1(16,2,1);
+    bf1.add("harel");
+    bf1.add("daniel");
+    bf1.add("tohar");
+    bf1.add("");
+    bf1.add("https://he.wikipedia.org/wiki/URL");
 }
-
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
