9b9b2edcea619caf45c0c8d1fcc9b97df5427acf
danieljenudi <danieljenudi@gmail.com>
2025-04-16 17:41:18 +0300

    finish the bloomFilter and hashFunc headers.

--- bloomFilter.cpp
+++ bloomFilter.cpp
@@ -1,34 +1,12 @@
 #include "bloomFilter.hpp"
 #include <stdexcept>
 
-bloomFilter::bloomFilter(int size, int hashCount1, int hashCount2) {
+bloomFilter::bloomFilter(int size, std::vector<hashFunc> &hashFunctions) {}
 
-    // Validate the parameters
-    if (size <= 0 || hashCount1 <= 0 || hashCount2 < 0) {
-        throw std::invalid_argument("Size and hash counts must be positive integers.");
-    }
-
-    // Initialize the member variables only after validation
-    this->size = size; // Set the size of the bloom filter
-    this->hashCount1 = hashCount1; // Set the number of hash1
-    this->hashCount2 = hashCount2; // Set the number of hash2 (default is 0)
-
-    bitVector = new std::vector<bool>(size, false); // Initialize the bit vector with size and set all bits to false
-    blackList = new std::vector<std::string>(); // Initialize the blacklist
-}
-
-bloomFilter::~bloomFilter() {
-    
-    delete bitVector; // Free the memory allocated for the bit vector
-    delete blackList; // Free the memory allocated for the blacklist
-}
+bloomFilter::~bloomFilter() {}
 
 void bloomFilter::add(const std::string &input) {}
 
-bool bloomFilter::isContains(const std::string &input) {return false;}
+bool bloomFilter::isContains(const std::string &input) const {return false;}
 
-bool bloomFilter::isInBlackList(const std::string &input) {return false;}
-
-bool bloomFilter::isValidForCreation(const std::string &input) {return false;}
-
-bool bloomFilter::isValidForOperation(const std::string &input) {return false;}
+bool bloomFilter::isInBlackList(const std::string &input) const {return false;}
