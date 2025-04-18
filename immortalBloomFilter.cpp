6d93d29ef18a47aee518b1ccf9e29d30ffbdf177
danieljenudi <danieljenudi@gmail.com>
2025-04-18 15:45:44 +0300

    add implamantation to runProgram. test after immortalBloomFilter Finish

--- immortalBloomFilter.cpp
+++ immortalBloomFilter.cpp
@@ -6,15 +6,18 @@ const std::string immortalBloomFilter::m_sizeFile = "";          // Name of the 
 const std::string immortalBloomFilter::m_hashFuncsFile = "";     
 const std::string immortalBloomFilter::m_blackListFile = "";      
 
-immortalBloomFilter::immortalBloomFilter(int size, std::vector<hashFunc> &hashFunctions)
+immortalBloomFilter::immortalBloomFilter(size_t size, std::vector<hashFunc> &hashFunctions)
  : bloomFilter(size, hashFunctions) {}
 
-immortalBloomFilter *immortalBloomFilter::createImmortalBloomFilter(int size, std::vector<hashFunc> &hashFunctions) {return nullptr;}
+immortalBloomFilter *immortalBloomFilter::createImmortalBloomFilter(size_t size, std::vector<hashFunc> &hashFunctions) 
+{return new immortalBloomFilter(size, hashFunctions);}
  
 immortalBloomFilter *immortalBloomFilter::reviveImmortalBloomFilter(){return nullptr;}
 
 immortalBloomFilter::~immortalBloomFilter() {}
 
-void immortalBloomFilter::add(const std::string &str) {}
+void immortalBloomFilter::add(const std::string &str) {
+    bloomFilter::add(str); // Call the base class add function
+}
 
 bool immortalBloomFilter::canBeRevive() {return false;}
--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
-#include "bloomFilter.hpp"
+#include "runProgram.hpp"
 #include <iostream>
 
 int main()
 {
     std::cout << "hi!" << std::endl;
+    runProgram rp;
+
+    rp.run();
     return 0;
 }
