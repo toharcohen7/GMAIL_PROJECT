4560773595525372228aca0c35983ba858f87246
danieljenudi <danieljenudi@gmail.com>
2025-05-06 21:26:28 +0300

    add the client python file

---
+++ client.py
@@ -0,0 +1,17 @@
+import socket # import socket module
+import sys    # import sys module
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create a TCP socket
+
+dest_ip = 'localhost'           
+
+dest_port = int(sys.argv[1])               
+s.connect((dest_ip, dest_port)) 
+
+while True: # run the loop for ever
+    msg = input() 
+    s.send(bytes(msg, 'utf-8'))    
+    data = s.recv(4096)            
+    print(data.decode('utf-8')) # decode the received data, which is in bytes, to a string
+
+s.close() # close the socket
