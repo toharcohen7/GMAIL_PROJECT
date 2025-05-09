5dc4ff63286063e36fbf4526e73470516228e979
danieljenudi <danieljenudi@gmail.com>
2025-05-09 16:38:25 +0300

    add input validation check to the server

--- src/main.cpp
+++ src/main.cpp
@@ -5,23 +5,30 @@
 #include "runProgram.hpp"        // runProgram class
 #include "server.hpp"           // server class
 
+#define SUCCESS 0
+#define ERROR 1 
+#define MIN_ARGUMENTS 4        // Minimum number of arguments required
+#define MAX_PORT_NUMBER 65535 // Maximum port number
+#define MIN_PORT_NUMBER 1024 // Minimum port number
+
+static bool isValidInut(int argc, char *argv[]);
+
 enum inputArguments {
     MAIN_FILE = 0,
     PORT_NUMBER = 1,
     BLOOM_FILTER_SIZE = 2,
+    FIRST_HASH_COUNT = 3,
     // Hash Count Start...
 };
 
 int main(int argc, char *argv[]) {
     
-
-    if (argc < 4) { // main file name + port number + bloom filter size + at least one hash count
-        throw std::runtime_error("not enough arguments\n");
-        return 1;
+    if(false == isValidInut(argc, argv)) { // Validate input arguments
+        return ERROR;
     }
 
     std::vector<int> hashCounts; // Vector to hold hash counts
-    for (int i = 3; i < argc; ++i) {
+    for (int i = FIRST_HASH_COUNT; i < argc; ++i) {
 
         hashCounts.push_back(std::stoi(std::string(argv[i]))); // Convert command line arguments to integers
     }    
@@ -40,5 +47,33 @@ int main(int argc, char *argv[]) {
     delete rp; // Clean up the runProgram object
     delete ibf; // Clean up the immortal bloom filter object
 
-    return 0;
+    return SUCCESS;
+}
+
+static bool isValidInut(int argc, char *argv[]) {
+
+    // main file name + port number + bloom filter size + at least one hash count
+    if (argc < MIN_ARGUMENTS) { 
+        return false; // Not enough arguments
+    }
+
+    if (MIN_PORT_NUMBER > std::stoi(std::string(argv[PORT_NUMBER])) || 
+        MAX_PORT_NUMBER < std::stoi(std::string(argv[PORT_NUMBER])) ) {
+        return false; // Invalid port number
+    }
+
+    if (0 >= std::stoi(std::string(argv[BLOOM_FILTER_SIZE]))) {
+        return false; // Invalid bloom filter size
+    }
+
+    for (int i = FIRST_HASH_COUNT; i < argc; ++i) {
+
+        // Check if hash count is valid
+        if (0 >= std::stoi(std::string(argv[i]))) { 
+
+            return false; // Invalid hash count
+        }
+    }
+
+    return true; // All input arguments are valid
 }
