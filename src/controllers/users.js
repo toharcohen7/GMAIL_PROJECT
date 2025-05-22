addd74ddb4fb429efbe9e1883ac78b37770400b1
harelMiz <harel10mizrahi@gmail.com>
2025-05-22 19:12:25 +0300

    adding users file

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -8,19 +8,14 @@ exports.getUserById= (req, res) => {
 }
 
 exports.createUser = (req, res) => {
-    const {userName, password,firstName,lastName,gender,birthDate} = req.body;
-    if(!userName)
-        return res.status(400).json({error: 'userName is required'});
-    if(!password)
-        return res.status(400).json({error: 'password is required'});
-     if(!firstName)
-        return res.status(400).json({error: 'firstName is required'});
-     if(!lastName)
-        return res.status(400).json({error: 'lastName is required'});
-     if(!gender)
-        return res.status(400).json({error: 'gender is required'});
-     if(!birthDate)
-        return res.status(400).json({error: 'birthDate is required'});
-    const newArticle = Articles.createUser(userName, password,firstName,lastName,gender,birthDate);
-    res.status(201).location(`/api/users/${newArticle.id}`).end();
-}
+    const requiredFields = ['userName', 'password', 'firstName', 'lastName', 'gender', 'birthDate'];
+    for (const field of requiredFields) {
+        if (!req.body[field]) {
+            return res.status(400).json({ error: `${field} is required` });
+        }
+    }
+
+    const { userName, password, firstName, lastName, gender, birthDate } = req.body;
+    const newUser = Users.createUser(userName, password, firstName, lastName, gender, birthDate);
+    res.status(201).location(`/api/users/${newUser.id}`).end();
+};
--- src/models/users.js
+++ src/models/users.js
@@ -3,6 +3,8 @@ const users = [];
 
 const getUser = (id) => {
     const user = users.find(u => u.id === id);
+    if (!user) 
+        return undefined;
     return {
         id: user.id,
         userName: user.userName,
@@ -15,7 +17,7 @@ const getUser = (id) => {
 
 const createUser = (userName, password,firstName,lastName,gender,birthDate) => {
     const newUser = {id: ++idCounter,userName, password,firstName,lastName,gender,birthDate};
-    user.push(newUser);
+    users.push(newUser);
     return newUser;
 };
 
