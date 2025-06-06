b67ac30f616605449f67ad3aa814f0da4b097306
danieljenudi <danieljenudi@gmail.com>
2025-06-06 13:37:31 +0300

    add the infrasracture fiels

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import SignIn from './SignIn/SignIn';
+
+function App() {
+  return (
+    <div className="App">
+      <SignIn />
+    </div>
+  );
+}
+
+export default App;
