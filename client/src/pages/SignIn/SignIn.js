f934213d69cfa88303282220aeb9840eb9968916
harelMiz <harel10mizrahi@gmail.com>
2025-06-13 21:02:21 +0300

    adding the jwt implementation

--- client/src/pages/SignIn/SignIn.js
+++ client/src/pages/SignIn/SignIn.js
@@ -29,7 +29,7 @@ function SignIn() {
         return response.json();
       })
       .then((data) => {
-        console.log("Token received:", data);
+        localStorage.setItem("token", data.token);
         setIsLoggedIn(true);
       })
       .catch((error) => {
--- src/controllers/tokens.js
+++ src/controllers/tokens.js
@@ -1,20 +1,20 @@
 const tokens = require('../models/tokens')
 
 exports.signIn = (req, res) => {
+  // Check if the user ID is provided and is a valid number
+  if (isThereExtraFields(req, ["userName", "password"])) {
+  return res.status(400).json({ error: 'No extra fields allowed' });
+  }
 
-    // Check if the user ID is provided and is a valid number
-    if (isThereExtraFields(req, ["userName", "password"])) {
-    return res.status(400).json({ error: 'No extra fields allowed' });
-    }
+  const { userName, password } = req.body
+  if (!userName || !password) 
+      return res.status(400).json({ error: 'User name and password are required' })
 
-    const { userName, password } = req.body
-    if (!userName || !password) 
-        return res.status(400).json({ error: 'User name and password are required' })
-
-    const token = tokens.signIn(userName, password)
-    if (!token) 
-        return res.status(401).json({ error: 'Invalid user name or password' })
-    res.set('X-User-Id', token.id).status(200).json({ id: token.id });
+  const jwtToken = tokens.signIn(userName, password);
+    if (!jwtToken)
+      return res.status(401).json({ error: 'Invalid user name or password' });
+  
+    res.status(200).json({ token: jwtToken });
 }
 
 function isThereExtraFields(req, expectedKeys) {
