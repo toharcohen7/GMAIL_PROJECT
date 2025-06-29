af6bcfb6cc348247d0fa308b422ad49a85b612d3
harelMiz <harel10mizrahi@gmail.com>
2025-06-29 19:05:58 +0300

    adding create mail implementation that communicate with server side

--- androidApp/app/src/main/java/com/example/gmail_app_dth/requests/MailUpdateRequest.java
+++ androidApp/app/src/main/java/com/example/gmail_app_dth/requests/MailUpdateRequest.java
@@ -1,14 +1,22 @@
 package com.example.gmail_app_dth.requests;
 
+import java.util.List;
+
 public class MailUpdateRequest {
     private final Boolean starred;
     private final Boolean onRead;
     private final String labelName;
+    private final String subject;
+    private final String content;
+    private final List<String> receiversNames;
 
-    public MailUpdateRequest(Boolean starred, Boolean onRead, String labelName) {
+    public MailUpdateRequest(Boolean starred, Boolean onRead, String labelName,String subject, String content, List<String> receiversNames) {
         this.starred = starred;
         this.onRead = onRead;
         this.labelName = labelName;
+        this.subject = subject;
+        this.content = content;
+        this.receiversNames = receiversNames;
     }
 
     public Boolean getStarred() {
@@ -22,4 +30,7 @@ public class MailUpdateRequest {
     public String getLabelName() {
         return labelName;
     }
+    public String getSubject() { return subject; }
+    public String getContent() { return content; }
+    public List<String> getReceiversNames() { return receiversNames; }
 }
