d691e5f3ead867950d540cfc735cea9b81f20624
danieljenudi <danieljenudi@gmail.com>
2025-07-01 12:55:02 +0300

    fix gradle, still need to fix room stuff

--- androidApp/app/src/main/java/com/example/gmail_app_dth/repository/UserRepository.java
+++ androidApp/app/src/main/java/com/example/gmail_app_dth/repository/UserRepository.java
@@ -32,7 +32,7 @@ public class UserRepository {
 
     public UserRepository(Context context) {
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://7d6d-79-181-175-112.ngrok-free.app/api/")
+                .baseUrl("http://10.0.2.2:12345/api/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
